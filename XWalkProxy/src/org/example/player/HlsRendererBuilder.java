141a3c685b1a204c5327c3f7eaa2838c43758dc0
fujunwei <junwei.fu@intel.com>
2016-06-14 16:22:49 +0800

    Update the version of ExoPlayer to 1.5.8 for fixing the issue about ZhanJiang TV can't be plaied

--- XWalkProxy/src/org/example/player/HlsRendererBuilder.java
+++ XWalkProxy/src/org/example/player/HlsRendererBuilder.java
@@ -160,7 +160,7 @@ public class HlsRendererBuilder implements RendererBuilder {
       // Build the video/id3 renderers.
       DataSource dataSource = new DefaultUriDataSource(context, bandwidthMeter, headers,
               false, proxyHost, proxyPort);
-      HlsChunkSource chunkSource = new HlsChunkSource(true /* isMaster */, dataSource, url,
+      HlsChunkSource chunkSource = new HlsChunkSource(true /* isMaster */, dataSource,
           manifest, DefaultHlsTrackSelector.newDefaultInstance(context), bandwidthMeter,
           timestampAdjusterProvider, HlsChunkSource.ADAPTIVE_MODE_SPLICE);
       HlsSampleSource sampleSource = new HlsSampleSource(chunkSource, loadControl,
@@ -177,7 +177,7 @@ public class HlsRendererBuilder implements RendererBuilder {
         DataSource audioDataSource = new DefaultUriDataSource(context, bandwidthMeter, headers,
                 false, proxyHost, proxyPort);
         HlsChunkSource audioChunkSource = new HlsChunkSource(false /* isMaster */, audioDataSource,
-            url, manifest, DefaultHlsTrackSelector.newAudioInstance(), bandwidthMeter,
+            manifest, DefaultHlsTrackSelector.newAudioInstance(), bandwidthMeter,
             timestampAdjusterProvider, HlsChunkSource.ADAPTIVE_MODE_SPLICE);
         HlsSampleSource audioSampleSource = new HlsSampleSource(audioChunkSource, loadControl,
             AUDIO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, mainHandler, player,
@@ -198,7 +198,7 @@ public class HlsRendererBuilder implements RendererBuilder {
         DataSource textDataSource = new DefaultUriDataSource(context, bandwidthMeter, headers,
                 false, proxyHost, proxyPort);
         HlsChunkSource textChunkSource = new HlsChunkSource(false /* isMaster */, textDataSource,
-            url, manifest, DefaultHlsTrackSelector.newSubtitleInstance(), bandwidthMeter,
+            manifest, DefaultHlsTrackSelector.newSubtitleInstance(), bandwidthMeter,
             timestampAdjusterProvider, HlsChunkSource.ADAPTIVE_MODE_SPLICE);
         HlsSampleSource textSampleSource = new HlsSampleSource(textChunkSource, loadControl,
             TEXT_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, mainHandler, player, DemoPlayer.TYPE_TEXT);
